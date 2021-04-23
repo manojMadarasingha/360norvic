b4f2eabbeba282996e22f233b2c7951935eefb57
ckat9988 <ckat9988@uni.sydney.edu.au>
2021-04-23 10:35:36 +1000

    uplodaded codes and processed dataset for DS_pkt

---
+++ DS_pkt_main_clf.py
@@ -0,0 +1,161 @@
+import settings
+import pandas as pd
+import numpy as np
+import os
+import pathlib
+
+import preprocess_data
+import DS_pkt_main_result_run_ml as run_ml
+import feature_importance
+from scipy import interpolate
+from sklearn.utils import shuffle
+import argparse
+
+
+def main_controller(platform, duration, path):
+    # select the traffic type
+    if platform == settings.PLATFORM_YT:
+        platform_folder = 'yt'
+    elif platform == settings.PLATFORM_FB:
+        platform_folder = 'fb'
+    else:
+        platform_folder = 'both'
+
+    output_path = path + '/' + 'clf_results' + '/' + platform_folder
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    if platform != settings.PLATFORM_BOTH:
+        path_in = path + '/' + 'processed_data_DS_pkt' + '/' + platform_folder
+        final_df = pd.read_csv(path_in + '/' + 's_1_w_5_t_' + str(duration) + '.csv', )
+    else:
+        path_in = path + '/' + 'processed_data_DS_pkt' + '/' + 'yt'
+        final_df_yt = pd.read_csv(path_in + '/' + 's_1_w_5_t_' + str(duration) + '.csv')
+        path_in = path + '/' + 'processed_data_DS_pkt' + '/' + 'fb'
+        final_df_fb = pd.read_csv(path_in + '/' + 's_1_w_5_t_' + str(duration) + '.csv')
+        final_df = pd.concat([final_df_yt, final_df_fb], axis=0)
+        final_df.reset_index(inplace=True, drop=True)
+
+    final_df = shuffle(final_df, random_state=123)
+
+    num_of_iterations = 20
+
+    roc_list = []
+    acc_list = []
+    prec_list = []
+    recal_list = []
+
+    for i in range(num_of_iterations):
+
+        train_df, test_df = run_ml.split_train_test(final_df, random_seed=i)
+
+        # remove ground truth variables
+        feature_removed = [
+            'Vid_pltform',
+            'Vid_pltform_ml',
+            'Vid_begin',
+            'Vid_end',
+            'loc',
+            'time',
+            'bandwidth',
+            'Vid_num',
+        ]
+
+        train_df.drop(feature_removed, inplace=True, axis=1)
+        test_df.drop(feature_removed, inplace=True, axis=1)
+
+        counts = np.asarray(test_df['Vid_type'].value_counts())
+
+        if len(counts) > 1:
+            acc, prec, recall, roc = run_ml.run_xgboost(train_df, test_df, platform,i)
+            roc_list.append(roc)
+            acc_list.append(acc)
+            prec_list.append(prec)
+            recal_list.append(recall)
+
+    final_performance = pd.DataFrame({'Acc': np.asarray(acc_list),
+                                      'Pred': np.asarray(prec_list),
+                                      'Reca': np.asarray(recal_list)})
+
+    print('average === Acc: %.2f  Prec: %.2f  Reca: %.2f ==='  % (np.mean(np.asarray(acc_list)),
+                                                                  np.mean(np.asarray(prec_list)),
+                                                                  np.mean(np.asarray(prec_list))))
+    # add different feature count for the data.
+    final_performance.to_csv(output_path + '/' + 's_1_w_5_t_' + str(duration) + '.csv')
+
+    return
+
+
+# interpolate the data
+def interpolate_auc(roc_list):
+    # store the values of auc
+    new_tpr_list = []
+    auc_val_list = []
+    new_fpr = np.arange(0, 1, 0.01)
+    for auc in roc_list:
+        auc_val_list.append(auc.loc[0, '_auc'])
+        fpr = auc['_fpr'].values
+        tpr = auc['_tpr'].values
+
+        f = interpolate.interp1d(fpr, tpr)
+        new_tpr_list.append(f(new_fpr))
+
+    new_tpr = np.asarray(new_tpr_list).mean(axis=0)
+    auc_val = np.asarray(auc_val_list).mean()
+    auc_val = np.repeat(auc_val, len(new_fpr))
+
+    final_auc_df = pd.DataFrame({'fpr': new_fpr,
+                                 'tpr': new_tpr,
+                                 'auc': auc_val})
+
+    final_auc_df.to_csv('/Users/ckat9988/Documents/Research/Passive_analaysis/Analysis/Experiments/temp/final_auc.csv')
+    return final_auc_df
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--t_type',
+                        type=str,
+                        default='YT',
+                        help='Select one of the traffic type: YT, FB or BOTH ')
+    parser.add_argument('--duration',
+                        type=int,
+                        default=60,
+                        help="Select onr of the duration of the trace: 20,30,60,90 or 120")
+    parser.add_argument('--path',
+                        type=str,
+                        default=os.path.abspath(os.getcwd()),
+                        help="Set the working dir path")
+
+    args = parser.parse_args()
+
+    # set default path to the current working directory.
+    pltform_str = args.t_type
+    if pltform_str == 'YT':
+        pltform = settings.PLATFORM_YT
+    elif pltform_str == 'FB':
+        pltform = settings.PLATFORM_FB
+    elif pltform_str == 'BOTH':
+        pltform = settings.PLATFORM_BOTH
+    else:
+        print('Enter valid Platform name')
+        return
+
+    duration = args.duration
+    if not duration in [20, 30, 60, 90, 120]:
+        print('Enter valid Duration')
+        return
+    else:
+        duration = args.duration
+    path = args.path
+
+    print('===Start running classification====')
+    main_controller(platform=pltform,
+                    duration=duration,
+                    path=path)
+
+    return
+
+
+if __name__ == main():
+    main()
---
+++ DS_pkt_main_result_run_ml.py
@@ -0,0 +1,229 @@
+import numpy as np
+from xgboost.sklearn import XGBClassifier
+from sklearn.utils import shuffle
+import settings
+import pandas as pd
+import assign_control_condition
+from sklearn.metrics import  roc_curve, roc_auc_score, \
+    average_precision_score, recall_score, accuracy_score
+
+def run_xgboost(df_train, df_test, platform,iter):
+
+    target = 'Vid_type'
+    predictors = [x for x in df_train.columns if x not in [target]]
+
+    x_train = df_train[predictors]
+    y_train = df_train[target]
+    x_test = df_test[predictors]
+    y_test = df_test[target]
+
+    # x_train = select_top_20_features(df=x_train,
+    #                                  platform=platform)
+    # x_test = select_top_20_features(df=x_test,
+    #                                 platform=platform)
+
+    xgboost_model = XGBClassifier(n_estimators=1000,
+                                  max_depth=7,
+                                  min_child_weight=1,
+                                  gamma=0,
+                                  objective='binary:logistic',
+                                  subsample=0.6,
+                                  colsample_bytree=0.6,
+                                  reg_lambda=1,
+                                  # reg_alpha=10,
+                                  scale_pos_weight=1.0,
+                                  verbosity=0)
+
+
+    xgboost_model.fit(x_train, y_train)
+    y_pred = xgboost_model.predict(x_test)
+
+
+    # store the predictions for each trace with their corresponding traces
+    accuracy = accuracy_score(y_test, y_pred)
+    precsion = average_precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred, average='binary')
+
+    # predict probabilities
+    model_probs = xgboost_model.predict_proba(x_test)
+    # keep probabilities for the positive outcome only
+    model_probs = model_probs[:, 1]
+    # calculate scores
+    model_auc = roc_auc_score(y_test, model_probs)
+    # summarize scores
+    # print('Logistic: ROC AUC=%.3f' % (model_auc))
+    # calculate roc curves
+    model_fpr, model_tpr, _ = roc_curve(y_test, model_probs)
+
+    roc_data = pd.DataFrame({'_fpr': model_fpr,
+                             '_tpr': model_tpr,
+                             '_auc': model_auc})
+
+    print('Iteration: %.2f === Acc: %.2f  Prec: %.2f  Reca: %.2f ==='  % (iter, accuracy, precsion, recall))
+
+    return accuracy, precsion, recall, roc_data
+
+
+# split the dataset
+def split_train_test(df, random_seed):
+    np.random.seed(random_seed)
+    all_videos = np.arange(0, 51, 1)
+    test_ind = np.random.choice(all_videos, size=15, replace=False)
+
+    train_ind = list(set(list(all_videos)) - set(list(test_ind)))
+
+    train_df = df.loc[df['Vid_num'].isin(list(train_ind))]
+    test_df = df.loc[df['Vid_num'].isin(list(test_ind))]
+
+    train_df = train_df[
+        (train_df['loc'] == assign_control_condition.Sydney) &
+        (train_df['bandwidth'] == assign_control_condition.Random)]
+
+    test_df = test_df[
+        (test_df['loc'] == assign_control_condition.Sydney) &
+        (test_df['bandwidth'] == assign_control_condition.Random)]
+
+    if test_df.shape[0] == 0:
+        test_ind = np.random.choice(train_ind, size=(len(train_ind)) // 4, replace=False)
+        train_ind = list(set(list(all_videos)) - set(list(test_ind)))
+
+        train_df.reset_index(inplace=True, drop=True)
+
+        test_df = train_df.loc[train_df['Vid_num'].isin(list(test_ind))]
+        train_df = train_df.loc[train_df['Vid_num'].isin(list(train_ind))]
+
+    train_df = shuffle(train_df, random_state=random_seed)
+    test_df = shuffle(test_df, random_state=random_seed)
+
+    return train_df, test_df
+
+
+# selec the top 20 features defined by the xgboost model
+def select_top_20_features(df, platform):
+    if platform == settings.PLATFORM_YT:
+        features = YT_W_5s_1s
+    elif platform == settings.PLATFORM_FB:
+        features = FB_W_5s_1s
+    else:
+        features = BOTH_W_5s_1s
+
+    df.reset_index(inplace=True, drop=True)
+
+    final_df_val = df[features].values
+
+    filtered_final_df = pd.DataFrame(data=final_df_val,
+                                     columns=features)
+    return filtered_final_df
+
+# most important features as is seen by the model
+YT_W_5s_1s = ['bytes_ip_dl_75%',
+              'bytes_frame_dl_75%',
+              'bytes_frame_dl_mean',
+              'packet_size_std_dl_50%',
+              'bytes_frame_dl_max',
+              'bytes_frame_dl_std',
+              'packet_size_std_dl_75%',
+              'packet_size_mean_dl_50%',
+              'bytes_ip_dl_max',
+              'bytes_hdr_dl_max',
+              'bytes_hdr_dl_std',
+              'packet_size_mean_ul_std',
+              'num_of_packets_dl_25%',
+              'bytes_frame_dl_50%',
+              'packet_size_std_dl_25%',
+              'packet_size_std_dl_max',
+              'packet_size_std_ul_mean',
+              'packet_size_max_dl_mean',
+              'bytes_frame_ul_min',
+              'packet_size_max_ul_min',
+              'packet_size_max_ul_mean',
+              'packet_size_std_ul_min',
+              'packet_size_max_ul_25%',
+              'bytes_hdr_dl_50%',
+              'packet_size_mean_ul_25%',
+              'bytes_hdr_dl_mean',
+              'bytes_hdr_dl_75%',
+              'bytes_frame_ul_75%',
+              'bytes_frame_dl_min',
+              'packet_size_min_dl_50%']
+
+FB_W_5s_1s = ['bytes_ip_dl_mean',
+              'packet_size_std_ul_25%',
+              'packet_size_std_ul_50%',
+              'packet_size_mean_ul_25%',
+              'bytes_ip_ul_mean',
+              'bytes_ip_ul_std',
+              'packet_size_std_ul_75%',
+              'bytes_frame_ul_std',
+              'bytes_frame_ul_max',
+              'bytes_hdr_dl_25%',
+              'bytes_ip_ul_max',
+              'packet_size_mean_ul_50%',
+              'packet_size_mean_dl_50%',
+              'packet_size_std_dl_50%',
+              'packet_size_mean_dl_max',
+              'bytes_ip_ul_50%',
+              'num_of_packets_ul_75%',
+              'packet_size_std_ul_min',
+              'bytes_frame_dl_std',
+              'packet_size_max_ul_75%',
+              'bytes_frame_ul_75%',
+              'packet_size_mean_ul_75%',
+              'packet_size_min_dl_max',
+              'packet_size_max_ul_mean',
+              'packet_size_min_dl_25%',
+              'bytes_frame_dl_25%',
+              'bytes_frame_dl_mean',
+              'packet_size_max_ul_min',
+              'bytes_frame_dl_max',
+              'packet_size_mean_dl_25%',
+              'num_of_packets_dl_max',
+              'packet_size_max_ul_50%',
+              'bytes_frame_ul_min',
+              'bytes_frame_dl_50%',
+              'bytes_hdr_ul_std',
+              'bytes_hdr_ul_max',
+              'packet_size_mean_ul_min',
+              'packet_size_std_ul_max',
+              'bytes_hdr_dl_std']
+
+BOTH_W_5s_1s = ['packet_size_std_ul_min',
+                'bytes_frame_dl_25%',
+                'packet_size_std_ul_75%',
+                'bytes_ip_dl_75%',
+                'bytes_frame_ul_max',
+                'packet_size_mean_dl_50%',
+                'packet_size_std_dl_75%',
+                'packet_size_mean_ul_25%',
+                'num_of_packets_dl_75%',
+                'packet_size_mean_dl_25%',
+                'packet_size_mean_ul_min',
+                'bytes_frame_dl_75%',
+                'bytes_frame_dl_mean',
+                'packet_size_mean_ul_50%',
+                'packet_size_mean_dl_75%',
+                'packet_size_max_ul_75%',
+                'bytes_frame_ul_std',
+                'bytes_ip_ul_max',
+                'packet_size_std_ul_50%',
+                'bytes_ip_ul_std',
+                'num_of_packets_dl_50%',
+                'packet_size_std_dl_50%',
+                'packet_size_max_ul_mean',
+                'packet_size_mean_dl_max',
+                'bytes_hdr_dl_50%',
+                'bytes_hdr_dl_max',
+                'packet_size_max_ul_25%',
+                'bytes_ip_dl_std',
+                'packet_size_std_dl_max',
+                'packet_size_min_dl_mean',
+                'bytes_frame_ul_50%',
+                'packet_size_mean_ul_mean',
+                'num_of_packets_ul_std',
+                'packet_size_min_ul_min',
+                'packet_size_min_ul_75%',
+                'packet_size_std_dl_25%',
+                'packet_size_mean_ul_75%',
+                'bytes_frame_dl_min',
+                'bytes_frame_ul_25%',
+                'packet_size_min_dl_25%']
